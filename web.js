47528ff02eb208195883e07376b2e549336e781a
ragaa91 <ragaa91@yahoo.com>
2013-07-07 12:21:30 +0000

    update1

--- web.js
+++ web.js
@@ -4,14 +4,13 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs=require('fs');
-fs.readFileSync('index.html', function (err, data) {
+var data=fs.readFileSync('index.html');
 var buf=new Buffer(data.length);
-if (err) throw err;
 for(i=0;i<data.length;i++)
 buf[i]=data[i];
   console.log(buf.toString('utf8', 0,buf.length));
-});
-  response.send(buf.toString('utf8', 0,buf.length));
+  
+response.send(buf.toString('utf8', 0,buf.length));
 });
 
 var port = process.env.PORT || 5000;
