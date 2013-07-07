80ebfee02b26a79745927791304dcfce5f90409b
ragaa91 <ragaa91@yahoo.com>
2013-07-07 12:08:19 +0000

    index updated

--- web.js
+++ web.js
@@ -3,7 +3,15 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var fs=require('fs');
+fs.readFileSync('index.html', function (err, data) {
+var buf=new Buffer(data.length);
+if (err) throw err;
+for(i=0;i<data.length;i++)
+buf[i]=data[i];
+  console.log(buf.toString('utf8', 0,buf.length));
+});
+  response.send(buf.toString('utf8', 0,buf.length));
 });
 
 var port = process.env.PORT || 5000;
